84ddcd5ffc05d4c0b240fd275d098e41d8fb8d33
AleeSaanta <101257437+AleeSaanta@users.noreply.github.com>
2022-03-21 14:20:53 +0100

    Personaje que salta y se mueve

--- Escena/Personatge/Personatge.gd
+++ Escena/Personatge/Personatge.gd
@@ -1,13 +1,23 @@
 extends KinematicBody2D
 
 
+
 var velocitat_base = 200
 var direccio = Vector2.DOWN
 var velocitat = Vector2.ZERO
 var gravetat = Vector2.DOWN * 980
+var velocitat_salt = -300
+
 
 func _physics_process(delta):
 	velocitat += gravetat * delta
-	move_and_slide(velocitat)
-
-
+	velocitat = move_and_slide(velocitat, Vector2.UP)
+	
+	velocitat.x = 0
+	velocitat += gravetat * delta
+	if Input.is_action_pressed("dreta"):
+		velocitat += Vector2.RIGHT * velocitat_base
+	if Input.is_action_pressed("esquerra"):
+		velocitat += Vector2.LEFT * velocitat_base
+	if Input.is_action_just_pressed("adalt") and is_on_floor():
+		velocitat.y = velocitat_salt
